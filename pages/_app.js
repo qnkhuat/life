ed9d1471b42a25e45158d2c8c000bfa80ad5ce00
Earther <qn.khuat@gmail.com>
2021-05-07 12:02:07 +0700

    no more dot env

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import 'tailwindcss/tailwind.css'
 import { AuthProvider } from '../lib/auth';
 import axios from "axios";
 axios.defaults.headers.post['Content-Type'] = 'application/json';
-require('dotenv').config()
 
 function MyApp({ Component, pageProps }) {
   return <AuthProvider>
