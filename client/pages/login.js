3d2f1d226565a0b8dbc00ba3839deca2849c07f8
Earther <qn.khuat@gmail.com>
2021-05-09 22:12:15 +0700

    fix jumping in the login page

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,21 +1,18 @@
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect } from "react";
 import { useAuth } from '../lib/firebase/auth';
 import Button from '@material-ui/core/Button';
 import axios from "axios";
 
 export default function Login() {
-  const { auth, user, signinWithGoogle } = useAuth();
+  const { auth, user, loading, signinWithGoogle } = useAuth();
   const router = useRouter();
-  if (auth) {
-    // First check whether user is added to database. if not direct to info page so you can add
-    if (!user) router.push(`/profile?next=${router.query.next}`); 
-    
-    // 2nd if has redirect request then driect there
-    // 3rd to home page as default
-    // Order matter here
-    router.push(router.query.next || '/'); 
-  }
+  useEffect(() => {
+    if(auth && !loading){
+      if (!user) router.push(`/profile?next=${router.query.next}`); 
+      router.push(router.query.next || '/'); 
+    }
+  }, [auth, user]);
 
   return (
     <>
